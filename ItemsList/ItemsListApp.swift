0359428aea0d870a01d7f9a4eacf2e36f039b422
Nour <nour.k@genie-soft.com>
2022-08-02 20:28:00 +0300

    Initial Commit

---
+++ ItemsList/ItemsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsListApp.swift
+//  ItemsList
+//
+//  Created by Nour Abukhaled on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ItemsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
